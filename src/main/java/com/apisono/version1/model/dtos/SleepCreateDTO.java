89bec11d7b6a2fdc88f00987ffc5b0282447548a
Alessandro Miguez <afmoreira@ufp.edu.pt>
2020-11-22 20:41:39 +0000

    added support for time serialization

--- src/main/java/com/apisono/version1/model/dtos/SleepCreateDTO.java
+++ src/main/java/com/apisono/version1/model/dtos/SleepCreateDTO.java
@@ -1,5 +1,6 @@
 package com.apisono.version1.model.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ public class SleepCreateDTO {
     private SleepStageDTO stages;
     private int totalMinutesAsleep;
     private int totalTimeInBed;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy mm:HH")
     private LocalDateTime date;
 
 }
--- src/main/java/com/apisono/version1/model/dtos/SleepDataDTO.java
+++ src/main/java/com/apisono/version1/model/dtos/SleepDataDTO.java
@@ -1,5 +1,6 @@
 package com.apisono.version1.model.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -15,6 +16,7 @@ public class SleepDataDTO {
     private int efficiency;
     private boolean isMainSleep;
     private int idLog;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy mm:HH")
     private LocalDateTime minuteData; //esta é uma lista de muitos dataTime não sei que atribuir
     private int minutesAfterWakeup;
     private int minutesAsleep;
@@ -22,6 +24,7 @@ public class SleepDataDTO {
     private int minutesToFallAsleep;
     private int restlessCount;
     private int restlessDuration;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
     private LocalDate startTime;
     private int timeInBed;
 
